7ca437eef2a7d42948f11320e642fae82db22e8a
Ibnballo1 <belloabdullateef035@gmail.com>
2024-12-08 08:38:57 +0100

    Add mobile navigation

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar';
+
+function Header() {
+  return (
+    <header>
+      <NavBar />
+      <h1>Logo</h1>
+    </header>
+  )
+}
+
+export default Header
