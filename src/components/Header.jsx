ea9123c6637780b50acf23b45badffc313306c8c
Ibnballo1 <belloabdullateef035@gmail.com>
2024-12-15 17:02:02 +0100

    Add mobile navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import NavBar from './NavBar';
+import logo from '../assets/images/mja-logo.webp';
+import '.././App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
     <header>
       <NavBar />
-      <h1>Logo</h1>
+      <div className='logo'><img src={logo} width={30} height={30} /></div>
+      {/* <div><FontAwesomeIcon icon={faEnvelope} /></div> */}
     </header>
   )
 }
